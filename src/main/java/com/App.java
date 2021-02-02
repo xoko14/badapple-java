813caf0ab33e0105b6446a9d03c65191f2b9e0e6
Xoko14 <xoquinperezb@gmail.com>
2021-02-02 12:40:31 +0100

    GZipBytes constructor seems to work

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,13 +1,12 @@
 package com;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        GZipBytes video = new GZipBytes(new File("./video/frames.bin.gz"));
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/com/GZipBytes.java
@@ -0,0 +1,38 @@
+package com;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import java.io.ByteArrayInputStream;
+import java.util.zip.GZIPInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class GZipBytes {
+    private byte[] bytes;
+    private byte[] compBytes;
+
+    GZipBytes(File file) {
+        try {
+            compBytes = FileUtils.readFileToByteArray(file);
+            ByteArrayInputStream bin = new ByteArrayInputStream(compBytes);
+            GZIPInputStream gzipper = new GZIPInputStream(bin);
+
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            int len;
+            while ((len = gzipper.read(buffer)) > 0) {
+                out.write(buffer, 0, len);
+            }
+
+            gzipper.close();
+            out.close();
+            bytes = out.toByteArray();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
